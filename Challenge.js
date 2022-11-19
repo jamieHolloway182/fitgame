88e5ffce614367272cf88e6cde2829480906f913
Fabiha1 <fabiha.8@hotmail.co.uk>
2022-11-19 15:29:08 +0000

    Update Challenge class

--- Challenge.js
+++ Challenge.js
@@ -1,15 +1,30 @@
 class Challenge{
     #name;
     #isCompleted;
-    constructor(name, isCompleted){
-        this.name = name;
-        this.isCompleted = isCompleted;
+    constructor(){
+        this.name = "";
+        this.isCompleted = false;
+        console.log("HEllo")
     }
     getName(){
         return this.name;
     }
     getIsCompleted(){
         return this.isCompleted;
+    }
+    setIsCompleted(isCompleted){
+        this.isCompleted = isCompleted;
+    }
+    getPoints(){
+        return this.points;
+    }
+    generateChallenge(){
+        let x = Math.floor((Math.random() * 100) + 1);
+        let y = Math.floor((Math.random() * 10) + 2);
+        let challenges = [`Travel ${x}m in ${y} minutes`, `Take ${x} steps in ${y} minutes`];
+        
+        this.name = challenges[Math.floor(Math.random() * (challenges.length + 1))];
+        return this.name;
     }
 
 }
