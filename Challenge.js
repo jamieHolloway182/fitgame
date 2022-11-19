350af34877fffbf742bad61a1a94971b5cae639c
Fabiha1 <fabiha.8@hotmail.co.uk>
2022-11-19 14:01:20 +0000

    Add challenge and car class

---
+++ Challenge.js
@@ -0,0 +1,15 @@
+class Challenge{
+    #name;
+    #isCompleted;
+    constructor(name, isCompleted){
+        this.name = name;
+        this.isCompleted = isCompleted;
+    }
+    getName(){
+        return this.name;
+    }
+    getIsCompleted(){
+        return this.isCompleted;
+    }
+
+}
