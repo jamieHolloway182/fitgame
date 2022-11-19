d1446010eaf427737b2a14a506523d61eefe12a0
fedeschuscheim <118612390+fedeschuscheim@users.noreply.github.com>
2022-11-19 19:29:34 +0000

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,19 +1,26 @@
 class Player{
     #totalPoints;
     #car;
+    #position
     constructor(){
         this.#totalPoints = 0;
         this.#car = new Car("red", 5);
+        this.#position = 1;
     }
     increasePoints(){
         // call when challenge complete
         this.#totalPoints += 10;
     }
     getTotalPoints(){
-        return this.totalPoints;
+        return this.#totalPoints;
     }
     getCar(){
-        return this.car;
+        return this.#car;
     }
-
+    setPosition(position){
+        this.#position = position;
+    }
+    getPosition(){
+        return this.#position;
+    }
 }
