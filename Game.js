b492d73e15f418e5a866f6ca8962871e9a2ff281
fedeschuscheim <118612390+fedeschuscheim@users.noreply.github.com>
2022-11-19 19:26:40 +0000

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,28 +1,44 @@
-class Game{
+class Game {
     #race;
     #challenges;
     #player;
-    #store;
-    constructor(){
-        this.challenges = [];
-        this.player = new Player;
-    }
-    startGame(){
-        
-    }
-    fillChallenges(){
-        challenge = new Challenge;
 
-        for(let i = 0; i < 4; i++){
-            this.challenges[i] = challenge.generateChallenge();
+    constructor() {
+        this.#challenges = [];
+        this.#player = new Player();
+        this.#race = new Race(100, 100, this.#player);
+    }
+    play() {
+        const time = 10;
+        let playerPosition = this.#player.getPosition();
+        while (!this.#race.checkIfFinished()) {
+            playerPosition = this.#race.getPlayerPosition(playerPosition);
+            setInterval(this.#race.moveCars(time), time);
         }
     }
-    challengeCompleted(){
+
+    fillChallenges() {
+        for (let i = 0; i < 4; i++) {
+            this.#challenges[i] = new Challenge();
+        }
+    }
+    challengeCompleted(challenge) {
         // if challenge is met
         // increase fuel level
-
+        this.#player.getCar().increaseFuelLevel();
         this.#player.increasePoints();
         // set challenge to complete
+        challenge.setIsCompleted();
+        replaceChallenge(challenge);
     }
-
+    replaceChallenge(challenge) {
+        found = false;
+        while (i < this.#challenges.length && !found == false) {
+            if (this.#challenges[i] == challenge) {
+                this.#challenges[i] = new Challenge();
+                found = true;
+            }
+            i++
+        }
+    }
 }
