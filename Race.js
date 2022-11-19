942ab97424bfaa0e0186890e4261c67e410239af
fedeschuscheim <118612390+fedeschuscheim@users.noreply.github.com>
2022-11-19 18:59:56 +0000

    Update Race.js

--- Race.js
+++ Race.js
@@ -1,22 +1,22 @@
-class Race{
-    #raceTrack;
+class Race {
+    #lapLength;
     #numberOfLaps;
     #cars;
 
-    constructor(raceTrack, numberOfLaps){
-        this.#raceTrack = raceTrack;
+    constructor(lapLength, numberOfLaps, player) {
+        this.#lapLength = lapLength;
         this.#numberOfLaps = numberOfLaps;
-        this.#cars = newArray();
-        this.#cars[0]=player.getCar();
+        this.#cars = [];
+        this.#cars[0] = player.getCar();
         this.addComputerCars();
-        for (i = 0; i < 3; i++){
+        for (i = 0; i < 3; i++) {
             this.#cars[i].setPosition(0);
             this.#cars[i].setLapNumber(0);
         }
     }
-    addComputerCars(){
-        for (i = 0; i < 2 ; i++){
-            const speed = Math.floor((Math.random()*20)+5);
+    addComputerCars() {
+        for (i = 0; i < 2; i++) {
+            const speed = Math.floor((Math.random() * 90) + 20);
             const colours = [];
             colours[0] = "red";
             colours[1] = "blue";
@@ -25,31 +25,60 @@ class Race{
             colours[4] = "green";
             colours[5] = "orange";
             colours[6] = "brown";
-            const colourRandomIndex = Math.floor((Math.random()*6));
-            const colour = colours [colourRandomIndex];
-            car = newCar(colour,speed);
+            const colourRandomIndex = Math.floor((Math.random() * 6));
+            const colour = colours[colourRandomIndex];
+            const car = newCar(speed, colour);
             this.#cars[i] = car;
         }
     }
-    moveCars(time){
-        for(i = 0; i < 3; i++){
-            if(this.#cars[i].getLapNumber() > this.#numberOfLaps){
-                if (this.#cars[i].getPosition() >= this.#raceTrack.getLength()){
+    moveCars(time) {
+        for (i = 0; i < 3; i++) {
+            if (this.#cars[i].getLapNumber() < this.#numberOfLaps) {
+                if (this.#cars[i].getPosition() == this.#lapLength) {
+                    this.#cars[i].increaseLapNumber();
+                    this.#cars[i].setPosition(0);
                     this.#cars[i].increasePosition(time);
                 }
+                else if (this.#cars[i].getPosition() < this.#lapLength) {
+                    this.#cars[i].increasePosition(time);
+                    if (this.#cars[i].getPosition() > this.#lapLength) {
+                        this.#cars[i].setPosition(this.#cars[i].getPosition() - this.#lapLength);
+                        this.#cars[i].increaseLapNumber();
+                    }
+                }
+            }
+            else if (this.#cars[i].getLapNumber() == this.#numberOfLaps) {
+
             }
         }
     }
-    getPlayerPosition(){
-        let playerPosition = 1;
-        for (i = 1 ; i < 3 ; i++){
-            if(this.#cars[i].getLapNumber() > this.#cars[0].getLapNumber()){
-                playerPosition ++;
-            } else if(this.#cars[i].getLapNumber() == this.#cars[0].getLapNumber()){
-                if(this.#cars[i].getPosition > this.#cars[0].getLapNumber()){
-                    playerPosition ++;
+    getPlayerPosition(playerPosition) {
+        let add = 0;
+        if (!this.#cars[0].getFinished()) {
+            for (i = 1; i < 3; i++) {
+                if (this.#cars[i].getFinished()) {
+                    add++;
+                } else if (this.#cars[i].getLapNumber() > this.#cars[0].getLapNumber()) {
+                    add++;
+                } else if (this.#cars[i].getLapNumber() == this.#cars[0].getLapNumber()) {
+                    if (this.#cars[i].getPosition() > this.#cars[0].getPosition()){
+                        add++;
+                    }
                 }
             }
+            return 1 + add;
         }
+        return playerPosition;
+    }
+    getCars() {
+        return this.#cars;
+    }
+    checkIfFinished() {
+        for (i = 0; i < this.#cars.length; i++) {
+            if (!cars[i].getFinished()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
